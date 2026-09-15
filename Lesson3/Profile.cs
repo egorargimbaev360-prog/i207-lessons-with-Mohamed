8d58a23a68d8658968ba5ad3e90815721a88a07c
25300627 <25300627@K216W22>
2026-09-15 13:22:49 +0500

    Добавьте файлы проекта.

---
+++ Lesson3/Profile.cs
@@ -0,0 +1,12 @@
+﻿namespace Lesson3;
+internal class Profile
+{
+    public int id = 50;
+    public string familyName = "Sallam";
+    public string surName = "Elsayid";
+    public string name = "Mohamed";
+    public DateTime birthDate = DateTime.Parse("02.02.1985");
+    internal double height = 170;
+    internal bool gender = false;
+}
+   
---
+++ Lesson3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson3
+{
+    internal class Program
+    {
+        static Lesson3.Profile clsProfile = new Lesson3.Profile();
+        static void Main(){
+            Console.WriteLine($"id:{clsProfile.id}\n" +
+                              $"Fullname: {clsProfile.name} {clsProfile.surName} {clsProfile.familyName}\n" +
+                              $"Age: {DateTime.Now.Year - clsProfile.birthDate.Year}\n" +
+                              $"BirthDate: {clsProfile.birthDate}\n" +
+                              $"Gender: {(clsProfile.gender == false ? "Man" : "Woman")}");
+                
+           
+        }
+    }
+}
